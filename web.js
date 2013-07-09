e59e7b4f0136516fef31df032ecb8bd4b84f34da
davet1985 <davethompson21@gmail.com>
2013-07-09 21:02:13 +0000

    buffer changes

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = Buffer.toString(fs.readFileSync("index.html"));
-  response.send(buffer);
+  var buffer = new Buffer(fs.readFileSync("index.html"));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
